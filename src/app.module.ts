94039de555dac239f8e0420f7ba307ede2d6884d
TALMO <talmoshel444@gmail.com>
2025-10-16 01:46:10 +0300

    fixed version for docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,37 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { typeormConfig } from './config/typeorm.config';
-// import { SongsModule } from './songs/songs.module';
-// import { UsersModule } from './users/users.module';
-// import { AuthModule } from './auth/auth.module';
-// import { BootstrapModule } from './bootstrap/bootstrap.module';
-// import { APP_GUARD } from '@nestjs/core';
-// import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
-// import { ServeStaticModule } from '@nestjs/serve-static';
-// import { join } from 'path';
-
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({ isGlobal: true }),
-//     TypeOrmModule.forRootAsync({
-//       inject: [ConfigService],
-//       useFactory: typeormConfig,
-//     }),
-//         ServeStaticModule.forRoot({
-//       rootPath: join(__dirname, '..', 'client'),
-//     }),
-//     SongsModule,
-//     UsersModule,
-//     AuthModule,
-//     BootstrapModule,
-//   ],
-//   providers: [
-//     { provide: APP_GUARD, useClass: JwtAuthGuard }, 
-//   ],
-// })
-// export class AppModule {}
-
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -54,7 +20,6 @@ import { join } from 'path';
       useFactory: typeormConfig,
     }),
 
-    // 👇 Serve the static frontend (React / client)
   ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
     }),
