c42ee03aa50ab1894a1617928686910df07b901f
Tal Moshel <talmoshel444@gmail.com>
2025-10-16 23:29:50 +0300

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,7 +8,7 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   async create(email: string, password: string) {
-    const user = this.repo.create({ email, password }); // use password (will be hashed automatically)
+    const user = this.repo.create({ email, password }); 
     return this.repo.save(user);
   }
 
